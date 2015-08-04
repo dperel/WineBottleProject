ed7b051e9e88d109bb05971d81e67efb27da789b
sarahsalder <sarahsalder@gmail.com>
2015-08-04 10:54:24 -0400

    updates views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
 end
