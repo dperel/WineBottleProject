180535b18549e92602214fb51d2d9340d6407536
sarahsalder <sarahsalder@gmail.com>
2015-08-04 16:13:03 -0400

    adds vineyard verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # helper_method :current_user
+
+  # def current_user
+  #   @current_user ||= session[:current_user_id] && User.find_by_id(session[:current_user_id])
+  # end
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :is_vineyard) }
