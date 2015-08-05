2795c99a295bdb5f7f9fc890130a78ceb12d1056
dperel <davidperel.pdf@gmail.com>
2015-08-05 15:17:08 -0400

    Sends btc between addresses upon sale of bottle to selected user.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -17,34 +17,34 @@ class Transaction < ActiveRecord::Base
 
   #who am i sending it to?
 
-  def make_transaction
-    sender_address = current_user.addresses.first
-    sender_priv_key = 'd3d135d1eabfa5510803ccc508cf66835bc321c7548d5e54a8679d26c18763c9'
-    receiver = Address_Maker.new
-    #User find by id.addresses << address_maker.new
-    receiver_address = receiver.address
-    puts "Receiver Address"
-    puts receiver_address
-    puts "Receiver Private Key"
-    puts receiver.priv_key
-    puts "Receiver Public Key"
-    puts receiver.pub_key
-    puts "Chain Client:"
-    chain_client.transact(
-      inputs: [ 
-        {
-          address: sender_address,
-          private_key: sender_priv_key
-        }
-        ],
-      outputs: [
-        {
-          address: receiver_address,
-          amount: 0.001
-        }
-      ]
-    )
-  end
+  # def make_transaction
+  #   sender_address = current_user.addresses.first
+  #   sender_priv_key = 'd3d135d1eabfa5510803ccc508cf66835bc321c7548d5e54a8679d26c18763c9'
+  #   receiver = Address_Maker.new
+  #   #User find by id.addresses << address_maker.new
+  #   receiver_address = receiver.address
+  #   puts "Receiver Address"
+  #   puts receiver_address
+  #   puts "Receiver Private Key"
+  #   puts receiver.priv_key
+  #   puts "Receiver Public Key"
+  #   puts receiver.pub_key
+  #   puts "Chain Client:"
+  #   chain_client.transact(
+  #     inputs: [ 
+  #       {
+  #         address: sender_address,
+  #         private_key: sender_priv_key
+  #       }
+  #       ],
+  #     outputs: [
+  #       {
+  #         address: receiver_address,
+  #         amount: 0.001
+  #       }
+  #     ]
+  #   )
+  # end
 
 def make_transaction(receiver_id, bottle_description, params)
   receiver_address(receiver_id, bottle_description)
@@ -63,7 +63,10 @@ end
 def transfer_balance(params, chain_client)
   sender_address = params[:address][:sending_btc_address]
   sender_private_key = params[:address][:sending_private_key]
-  chain_client.transact(
+  new_chain_client = Chain::Client.new(key_id: 'a9c71386c1e7d619906f66c1ad13f01d', key_secret: '781ed927e326b189e90c5ad94e1e25f3')
+  new_chain_client.block_chain = 'testnet3'
+  new_chain_client.transact(
+
       inputs: [ 
         {
           address: sender_address,
@@ -73,7 +76,7 @@ def transfer_balance(params, chain_client)
       outputs: [
         {
           address: @address.btc_address,
-          amount: 0.001
+          amount: 0.00001
         }
       ]
     )
