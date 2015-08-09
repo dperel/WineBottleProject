52159edc583b651ea51e829920b06e5e825d897c
dperel <davidperel.pdf@gmail.com>
2015-08-09 11:45:27 -0400

    Adds 'Find Me on the Blockchain' link to each bottle on the show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808204700) do
+ActiveRecord::Schema.define(version: 20150809150243) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150808204700) do
     t.string   "brand_name"
     t.string   "provenance"
     t.string   "stringified_description"
+    t.string   "url"
   end
 
   create_table "histories", force: :cascade do |t|
