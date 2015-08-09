bcbd36cf6f549e0b538cba688234c8970253ac5c
AnnaErshova <anna.ershova@gmail.com>
2015-08-08 23:43:56 -0400

    replaces passwords with Figaro replacements

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 
-User.create([:name => 'Sarah', :email => 'sarah@sarah.com', :password => 'sarah123', :password_confirmation => 'sarah123', :is_vineyard => 1])
-User.create([:name => 'David', :email => 'david@david.com', :password => 'david123', :password_confirmation => 'david123', :is_vineyard => 1])
-User.create([:name => 'Anna', :email => 'anna@anna.com', :password => 'annae123', :password_confirmation => 'annae123', :is_vineyard => 0])
+User.create([:name => 'Sarah', :email => 'sarah@sarah.com', :password => 'sarah123', :password_confirmation => ENV[sarahs_fake_password], :is_vineyard => 1])
+User.create([:name => 'David', :email => 'david@david.com', :password => 'david123', :password_confirmation => ENV[davids_fake_password], :is_vineyard => 1])
+User.create([:name => 'Anna', :email => 'anna@anna.com', :password => 'annae123', :password_confirmation => ENV[annas_fake_password], :is_vineyard => 0])
