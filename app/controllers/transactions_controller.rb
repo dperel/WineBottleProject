81bb6d0b93f866bf657099f00e20fc922d477280
dperel <davidperel.pdf@gmail.com>
2015-08-12 12:36:59 -0400

    Flashes error message if bottle not yet confirmed in blockchain

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,18 +1,24 @@
 class TransactionsController < ApplicationController
 
   def create
-    @transaction = Transaction.new
-    if params[:address][:user_id].blank? # cannot sell to no listed user
-      flash[:notice] = "Please choose a user."
-    else # if there is a valid recipient
+      @transaction = Transaction.new
       previous_address = Address.where(btc_address: params[:address][:sending_btc_address]).first
-      recipient = User.find(params[:address][:user_id])
-      @address = Address.new
-      @address.generate_btc_address_and_keys
-      @transaction.make_transaction(previous_address, recipient, params)
-      flash[:notice] = "Successfully sold!"
-    end
-    redirect_to user_path(current_user) 
-  end
-
+      previous_btc = previous_address.btc_address
+      if Transaction.get_balance(previous_btc)
+        if params[:address][:user_id].blank? # cannot sell to no listed user
+          flash[:notice] = "Please choose a user."
+        else # if there is a valid recipient
+          recipient = User.find(params[:address][:user_id])
+          @address = Address.new
+          @address.generate_btc_address_and_keys
+          @transaction.make_transaction(previous_address, recipient, params)
+          redirect_to request.referrer
+          # redirect_to root_path
+        end 
+      else 
+        flash[:message] = "This bottle has not been entered into the blockchain yet. Please wait a couple of minutes."
+        redirect_to request.referrer
+      end 
+  end 
 end
+
