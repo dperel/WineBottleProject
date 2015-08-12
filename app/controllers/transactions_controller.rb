5c3e5d1ff3d58566ca9a7655214ba65c1311b52a
AnnaErshova <anna.ershova@gmail.com>
2015-08-11 23:34:28 -0400

    re-factors Transactions Controller further

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,8 +2,7 @@ class TransactionsController < ApplicationController
 
   def create
     @transaction = Transaction.new
-    binding.pry
-    if params[:address][:user_id].present? # cannot sell to no listed user
+    if params[:address][:user_id].blank? # cannot sell to no listed user
       flash[:notice] = "Please choose a user."
     else # if there is a valid recipient
       previous_address = Address.where(btc_address: params[:address][:sending_btc_address]).first
