1fd16cbfd1b3c0170bb5433bc63edcb095c2d70b
AnnaErshova <anna.ershova@gmail.com>
2015-08-13 21:58:48 -0400

    separate generate_bitcoin_keys into a new method

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,6 @@
 class AddressesController < ApplicationController
 
-  require 'bitcoin'
-  Bitcoin.network = :testnet3
+  before_create :set_bitcoin_network
 
   def create  # following pressing 'register a new bottle' and then 'submit'
      @address = Address.new
@@ -29,27 +28,32 @@ class AddressesController < ApplicationController
          "#{@address.vintage} #{@address.vineyard_name} #{@address.wine_type} from #{@address.provenance}."
      end
  
-     key_pair = Bitcoin::generate_key
-     @address.private_key = key_pair[0]
-     @address.public_key = key_pair[1]
-     @address.btc_address = Bitcoin::pubkey_to_address(key_pair[1])
- 
-     @address.save
-     new_address = @address
-     WineFaucet.transfer_balance(new_address)
-     redirect_to user_path(current_user)
-
+    generate_bitcoin_keys
+    new_address = @address
+    WineFaucet.transfer_balance(new_address)
+    redirect_to user_path(current_user)
   end
 
   def index
     @user_addresses = Addresses.find(params[:user_id])
   end
 
+  def generate_bitcoin_keys
+    key_pair = Bitcoin::generate_key
+    @address.private_key = key_pair[0]
+    @address.public_key = key_pair[1]
+    @address.btc_address = Bitcoin::pubkey_to_address(key_pair[1])
+    @address.save
+  end
 
   private
 
   def address_params
     params.require(:address).permit(:avatar)
+  end
+
+  def set_bitcoin_network
+    Bitcoin.network = :testnet3
   end
 
 end
