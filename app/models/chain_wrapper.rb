e8e7077570f3628fdbe8d3e5b7f6a5fbbe528e81
dperel <davidperel.pdf@gmail.com>
2015-08-17 22:10:00 -0400

    Added key to chain wrapper

--- app/models/chain_wrapper.rb
+++ app/models/chain_wrapper.rb
@@ -11,7 +11,7 @@ class ChainWrapper
   end
 
   def self.set_chain_client_and_testnet
-    @chain_client = Chain::Client.new(key_id: '0465c861f92619e87714a3f1b9bad5bb', key_secret: ENV["key_secret"])
+    @chain_client = Chain::Client.new(key_id: '0465c861f92619e87714a3f1b9bad5bb', key_secret: '9ceed24576bab83b8b18a9bd4997faa0')
     @chain_client.block_chain = 'testnet3'
   end
 
