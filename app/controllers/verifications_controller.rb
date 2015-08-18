eb69ced7de9c0886cca58d9f55b2c7ab8024457b
dperel <davidperel.pdf@gmail.com>
2015-08-18 14:53:44 -0400

    changed quotation marks

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -3,7 +3,7 @@ class VerificationsController < ApplicationController
   def create
     address = params["address"]["sending_btc_address"]
     chain_client = Chain::Client.new(key_id: '0465c861f92619e87714a3f1b9bad5bb', 
-                                     key_secret: ENV["key_secret"])
+                                     key_secret: ENV['key_secret'])
     chain_client.block_chain = 'testnet3'
     @response = chain_client.get_address(address)
     @address_balance = @response[0]["confirmed"]["balance"]
